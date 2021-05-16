3a2d61c26a34006aa0ab35d03de91d9fc7827d4d
Flaymed <markcviggiano@gmail.com>
2021-05-15 23:28:17 -0400

    Events Structure

--- public/js/events.js
+++ public/js/events.js
@@ -1,14 +1,26 @@
 let events = new Map();
+let searchBox = document.getElementById("eventsFilter");
 
 class Event {
-  constructor(name, imgPath ) {
+  constructor(name, imgPath, about, hasContent, id) {
     this.name = name;
     this.img = imgPath;
+    this.about = about;
+    this.hasContent = hasContent;
+    this.id = id;
   }
 }
 
+function filter() {
+  let text = searchBox.value;
+}
+
 window.addEventListener('load', (event) => {
-  fetch("./api/events").then((response) => {
-    console.log(response);
+  fetch("./api/events")
+  .then(response => response.json())
+  .then((events) => {
+    for (const event of events) {
+      console.log(event);
+    }
   })
 })
