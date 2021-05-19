df2d0cecda39c8c06d063f2daa7d67a3f631fd3d
Flaymed <markcviggiano@gmail.com>
2021-05-19 11:28:35 -0400

    Staff Frontend

--- routes/api/staff.js
+++ routes/api/staff.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database("./database/database.db");
 
 router.get('/', (req, res) => {
-  res.send("Staff API route.");
+
+  let users = [];
+  db.each("SELECT * FROM users", (error, user) => {
+    userData = {
+      email: user.email,
+      imgPath: user.imgPath,
+      name: user.name
+    }
+    users.push(userData);
+  }, () => {
+    res.send(users);
+  })
+
+
 });
 
 module.exports = router;
